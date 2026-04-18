8f8cf978a31a1e348dac7aa0fb85dac11b89441a
76182 <ojablon1@stu.vistula.edu.pl>
2026-04-18 13:05:21 +0200

    Add files via upload

---
+++ zad5_form_walidacja/script.js
@@ -0,0 +1,74 @@
+console.log("DZIALA JS");
+
+const buttonTheme = document.getElementById("zmianaMotywu");
+const buttonToggle = document.getElementById("sectionVisibility");
+const link = document.getElementById("theme");
+const section = document.getElementById("doswiadczenie");
+
+section.style.display = "block";
+
+
+buttonTheme.addEventListener("click", function () {
+    if (link.getAttribute("href") === "red.css") {
+        link.setAttribute("href", "green.css");
+    } else {
+        link.setAttribute("href", "red.css");
+    }
+});
+
+buttonToggle.addEventListener("click", function () {
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+});
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let isValid = true;
+
+
+    const fname = document.getElementById("fname");
+    const lname = document.getElementById("lname");
+    const email = document.getElementById("email");
+    const zgoda = document.getElementById("zgoda");
+
+    // ukrycie b³êdów
+    document.getElementById("error1").style.display = "none";
+    document.getElementById("error2").style.display = "none";
+    document.getElementById("error3").style.display = "none";
+    document.getElementById("error4").style.display = "none";
+
+
+    // imiê
+    if (fname.value === "" || /[0-9]/.test(fname.value)) {
+        document.getElementById("error1").style.display = "block";
+        isValid = false;
+    }
+
+    // nazwisko
+    if (lname.value === "" || /[0-9]/.test(lname.value)) {
+        document.getElementById("error2").style.display = "block";
+        isValid = false;
+    }
+
+    // email
+    if (email.value === "" || email.value.includes("@") === false) {
+        document.getElementById("error3").style.display = "block";
+        isValid = false;
+    }
+
+    // checkbox
+    if (zgoda.checked === false) {
+        document.getElementById("error4").style.display = "block";
+        isValid = false;
+    }
+
+    if (isValid === true) {
+        alert("Formularz wyslany poprawnie!")
+    }
+});
